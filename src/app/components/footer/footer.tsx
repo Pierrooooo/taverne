637cac58f9562b4d131f5f67639fc67f3268744f
pierro <pierrecaillet2811@gmail.com>
2025-01-09 17:25:30 +0100

    💫  Footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,48 +1,66 @@
-'use client'
+'use client';
 
 import Image from "next/image";
 import Link from 'next/link';
 import styles from './footer.module.css';
 import CopyToClipboard from "@/app/utils/copyToClipboard";
+import { useEffect } from "react";
+import { scrollReveal, slideDown } from "@/app/animations/animationsgsap";
 
 export default function Footer(): JSX.Element {
 
+    useEffect(() => {
+        const footerTitles = ['title1', 'title2']
+        footerTitles.forEach((title) => {
+            scrollReveal(
+                `footer #${title}`,
+                `footer #${title}`
+            );
+        })
+
+        const liIds = ['footer_nav_li1','footer_nav_li2','footer_nav_li3', 'footer_contact_li1','footer_contact_li2','footer_contact_li3', 'footer_hours_li'];
+        liIds.forEach((id) => {
+            scrollReveal(
+                `footer #${id}`,
+                `footer #${id}`
+            );
+        });
+    }, []);
+
     return (
-        <footer className={styles.footer}>
+        <footer className={`${styles.footer} footer`}>
             <div className={`${styles.footer_infos} small_container`}>
                 <div className={styles.footer_info}>
-                    <p className="title">Navigation</p>
+                    <p id="title1" className="title">Navigation</p>
                     <ul>
-                        <li>
+                        <li id="footer_nav_li1">
                             <Link href="/">Accueil</Link>
                         </li>
-                        <li>
+                        <li id="footer_nav_li2">
                             <Link href="/menu">Menu</Link>
                         </li>
-                        <li>
+                        <li id="footer_nav_li3">
                             <Link href="/gallery">Gallerie</Link>
                         </li>
                     </ul>
                 </div>
                 <div className={styles.footer_info}>
-                    <p className="title">Contact</p>
+                    <p id="title2" className="title">Contact</p>
                     <ul>
-                        <li>
+                        <li id="footer_contact_li1">
                             <CopyToClipboard type="phone" text="09 79 39 11 01"/>
                         </li>
-                        <li>
+                        <li id="footer_contact_li2">
                             <CopyToClipboard text="lerestautdehervé@gmail.com"/>
                         </li>
-                        <li>
+                        <li id="footer_contact_li3">
                             <a target='_blank' href="https://www.google.fr/maps/place/La+P'tite+Taverne/@48.9683424,2.1600307,14.36z/data=!4m10!1m2!2m1!1sla+pt'ite+tavern!3m6!1s0x47e661a0d6113b35:0x597b80010c1c8d95!8m2!3d48.9704529!4d2.1806255!15sChBsYSBwJ3RpdGUgdGF2ZXJuWhIiEGxhIHAgdGl0ZSB0YXZlcm6SARFmcmVuY2hfcmVzdGF1cmFudOABAA!16s%2Fg%2F11y9r4ljlz?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D">21 Quai de Seine, 
                             95530 La Frette-sur-Seine</a>
                         </li>
-                        <li>Ouvert du mardi au dimanche de 17h à 23h</li>
+                        <li id="footer_hours_li">Ouvert du mardi au dimanche de 17h à 23h</li>
                     </ul>
                 </div>
-
             </div>
-            {/* <p className={styles.test_text}>La p&apos;tite Taverne</p> */}
             <div className={styles.footer_image}>
                 <Image
                     height={300}
